f0410a9fbae2c58d5d597d724be39ea494743041
Tara <taraplatt88@gmail.com>
2024-03-18 20:26:05 -0700

    button logs

--- app.js
+++ app.js
@@ -1,1 +1,22 @@
-console.log("this is hard")
+// console.log("this is hard")
+
+// function clicked() {
+// console.log('Button Clicked!')
+// }
+
+
+
+
+
+let home = 0
+let away = 0
+
+function increaseHome() {
+    home += 1
+    console.log('⚽️', home);
+}
+
+function increaseAway() {
+    away += 1
+    console.log('🥅', away);
+}
